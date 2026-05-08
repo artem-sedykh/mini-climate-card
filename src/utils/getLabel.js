33a5ec6f2cef1c543676dad63f3d947f87551388
wolfie6 <37439179+wolfie6@users.noreply.github.com>
2026-05-08 22:38:34 +0200

    Update getLabel.js use localize() instead of resource
    https://github.com/home-assistant/frontend/pull/51676

--- src/utils/getLabel.js
+++ src/utils/getLabel.js
@@ -1,13 +1,9 @@
 const getLabel = (hass, labels, fallback = 'unknown') => {
-  const lang = hass.selectedLanguage || hass.language;
-  const resources = hass.resources[lang];
-  if (!resources)
-    return fallback;
-
   for (let i = 0; i < labels.length; i += 1) {
     const label = labels[i];
-    if (resources[label])
-      return resources[label];
+	const resourceLabel = hass.localize(label);
+    if (resourceLabel !== '')
+      return resourceLabel;
   }
 
   return fallback;
