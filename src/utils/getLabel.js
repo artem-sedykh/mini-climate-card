6da40127a5e87f67c697c4033caa036ff8a39df3
wolfie6 <37439179+wolfie6@users.noreply.github.com>
2026-05-11 09:50:17 +0200

    Update getLabel.js

--- src/utils/getLabel.js
+++ src/utils/getLabel.js
@@ -2,7 +2,7 @@ const getLabel = (hass, labels, fallback = 'unknown') => {
   for (let i = 0; i < labels.length; i += 1) {
     const label = labels[i];
     const resourceLabel = hass.localize(label);
-    
+
     if (resourceLabel !== '')
       return resourceLabel;
   }
